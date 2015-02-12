b53ab6ebb2010fce18e6a77ccdd7ca5ffeba276c
Nenad Nikolic <nnikolic87@gmail.com>
2015-02-12 19:17:12 +0100

    adding authentication features

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,6 +9,26 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: 'home/_home.html',
       controller: 'MainCtrl'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'auth/_login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'auth/_register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
     });
 
   $urlRouterProvider.otherwise('home');
