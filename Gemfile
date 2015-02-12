e70f252cf8d59a382bc2fcdea7823e6ae6b29358
Nenad Nikolic <nnikolic87@gmail.com>
2015-02-12 19:04:31 +0100

    authentication with devise initial setup, devise gem and angular-devise lib

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.0'
 gem 'angular-rails-templates'
+gem 'devise'
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,4 @@
-angular.module('startup', ['ui.router', 'templates'])
+angular.module('startup', ['ui.router', 'templates', 'Devise'])
 .config([
 '$stateProvider',
 '$urlRouterProvider',
