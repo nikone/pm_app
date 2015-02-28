ba1e147a247712727e51aee6453e209d5dd51976
Nenad Nikolic <nnikolic87@gmail.com>
2015-02-28 17:14:56 +0100

    adding tasks and board models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228160607) do
+ActiveRecord::Schema.define(version: 20150228161235) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -24,6 +30,12 @@ ActiveRecord::Schema.define(version: 20150228160607) do
     t.string   "description", limit: 255
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "users", force: :cascade do |t|
