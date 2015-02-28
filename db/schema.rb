4e06eacf232b91586b462a8c0fb5628de5332c5d
Nenad Nikolic <nnikolic87@gmail.com>
2015-02-28 17:09:33 +0100

    adding companies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212195911) do
+ActiveRecord::Schema.define(version: 20150228160607) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title",       limit: 255
