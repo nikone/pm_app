3b9602d2f7eb126b1b12628f8be03a2f9d6853e9
Nenad Nikolic <nnikolic87@gmail.com>
2015-03-01 15:56:27 +0100

    board has many tasks

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,4 +2,5 @@ class Board < ActiveRecord::Base
   validates :title, presence: true
 
   belongs_to :project
+  has_many :tasks
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Board, type: :model do
   it { expect(subject).to validate_presence_of(:title) }
 
   it { expect(subject).to belong_to(:project) }
+  it { expect(subject).to have_many(:tasks) }
 end
